1d5006cfb8226f3774405260dc2fbb9a6d958a6c
Nikolaipe <nikolaipe@getacademy.no>
2022-11-04 12:26:08 +0100

    Ikke så værst
    Gidder ikke pynte på

---
+++ script.js
@@ -0,0 +1,32 @@
+//model
+let bulb = 'bulbOff';
+let room = 'black';
+let lightSwitch = '<img src="img/off.png"/>';
+let illumination = 'off';
+
+//view
+showContent();
+function showContent(){
+    document.getElementById('page').innerHTML = /*html*/`
+    <!-- lyspæra -->
+    <div class ="${bulb}"></div>
+    <div class ="socket"></div>
+    <div class = "switch" onclick="switchLight()"> ${lightSwitch} </div>
+    `;
+    document.body.style.backgroundColor = room;
+}
+//controller
+function switchLight(){
+    if(illumination == 'off'){
+        room = 'white';
+        bulb = 'bulbOn';
+        illumination = 'on'
+        lightSwitch = '<img src="img/on.png"/>'
+    }else {
+        room = 'black';
+        bulb = 'bulbOff';
+        illumination = 'off'
+        lightSwitch = '<img src="img/off.png"/>'
+    }
+    showContent();
+}
